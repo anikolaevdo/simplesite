1b4a9dd963d845ce63686d4fc365cd445497ca6b
Николаев Антон Андреевич <Naa@Medincom.local>
2020-03-03 11:23:34 +0500

    Добавлена навигация по рубрикам

--- bboard/models.py
+++ bboard/models.py
@@ -2,7 +2,28 @@ from django.db import models
 
 
 class Bb(models.Model):
-    title = models.CharField(max_length=35)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
-    published = models.DateTimeField(auto_now_add=True, db_index=True)
+    title = models.CharField(max_length=35, verbose_name='Товар')
+    content = models.TextField(null=True, blank=True, verbose_name='Описание')
+    price = models.FloatField(null=True, blank=True, verbose_name='Цена')
+    published = models.DateTimeField(
+        auto_now_add=True, db_index=True, verbose_name='Опубликованно')
+    rubric = models.ForeignKey(
+        'Rubric', null=True, on_delete=models.PROTECT, verbose_name='Рубрика')
+
+    class Meta:
+        verbose_name_plural = 'Обьявление'
+        verbose_name = 'Обьявление'
+        ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(max_length=20, db_index=True,
+                            verbose_name='Название')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
--- bboard/views.py
+++ bboard/views.py
@@ -2,8 +2,20 @@ from django.shortcuts import render
 
 
 from .models import Bb
+from .models import Rubric
 
 
 def index(request):
-    bbs = Bb.objects.order_by('-published')
-    return render(request, 'bboard/index.html', {'bbs': bbs})
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return render(request, 'bboard/index.html', context)
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.object.filter(rubric=rubric_id)
+    rubrics = Rubric.object.all()
+    current_rubric = Rubric.object.get(pk=rubric_id)
+    context = {'bbs': bbs, 'rubrics': rubrics,
+               'current_rubric': current_rubric}
+    return render(request, 'bboard/by_rubric.html', context)
